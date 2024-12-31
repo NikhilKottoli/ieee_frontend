006af7678218afb51559eb685da216b845343704
NikhilKottoli <154459496+NikhilKottoli@users.noreply.github.com>
2024-12-31 16:39:20 +0530

    Update verify-files.js

--- verify-files.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-console.log('Current directory:', process.cwd());
-console.log('Directory contents:', fs.readdirSync('.'));
-console.log('Public directory contents:', fs.readdirSync('./public'));
